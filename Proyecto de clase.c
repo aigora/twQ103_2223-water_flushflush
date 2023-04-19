0764c9c171fad5e4a1729c435a70086a7b915be6
klausitum <130643702+klausitum@users.noreply.github.com>
2023-04-19 13:50:44 +0200

    mejora

--- Proyecto de clase.c
+++ Proyecto de clase.c
@@ -43,78 +43,31 @@ float porcentaje;
            	return 0;
 		   }
    
-     //Buscador alicia todavia no funcionA
-     /*	while(fscanf(ficheros,"%s %f %d %d %d ",fuente[i].parametros,&fuente[i].PH,&fuente[i].conductividad,&fuente[i].turbidez,&fuente[i].coliformes) !=EOF){
-	 do{ // El bucle do-while va a contener toda las programciones hasta el final, de manera que el usuario pueda hacer la b?squedad varias veces.
-		while(fscanf(ficheros,"%s %f %d %d %d ",fuente[i].parametros,&fuente[i].PH,&fuente[i].conductividad,&fuente[i].turbidez,&fuente[i].coliformes) !=EOF){//bUCLE HASTA el fin del fichero,cuando no sabes su tama?o
-
-			printf("1-Buscar por el nombre de la fuente.\n2-Buscar por un intervalo de pH.\n3-Salir del programa.\n");	// !!!! Hacemos con n?meros o con letras?	
-			printf("Introduzca la opci?n deseada: ");
-			scanf("%d", &opcion);
-			   switch (opcion) {
-			case 1:
-				printf("Introducir el nombre de la fuente: ");
-				scanf("%s", nombrebuscar);
-				
-				for(i=0; i<Tam_Max; i++) {
-					if(strcmp(fuente[i].nombre_fuente,nombrebuscar)==0) {
-						fuente_encontrado=1;
-						printf("-----Fuente-----\n");
-						printf("Nombre: %s\n", fuente[i].nombre_fuente);
-						printf("pH: %.2f\n", fuente[i].PH);
-						printf("Conductividad (microS/cm): %d\n", fuente[i].conductividad);
-						printf("Turbidez (NTU): %d\n", fuente[i].turbidez);
-						printf("Coliforme (UFC/100ml): %d\n", fuente[i].coliformes);
-						// A?adir aqu? las otras caracter?sticas de la fuente: potable....
-					} 
-					if(fuente_encontrado == 0) {
-						printf("El fuente %s no existe.\n", nombrebuscar); // ???? Que salga del programa (elegir otra vez la opcion) o que vuelva a introducir el nombre ???
-					}
-				}
-				
-				
-				break;
-			case 2:
-				printf("Introducir el intervalo de pH: desde ");
-				scanf("%d", &pH_inicio);
-				printf("hasta ");
-				scanf("%d", &pH_final);
-				
-				
-				break;
-			case 3:
-				printf("Has salido del programa.\n");
-				break;
-			default:
-				printf("Opcion incorrecta.\n");
-			   
-			   
-			   
-			   
-			   
-			   
-			   i++;
-		}
-		
-		
-		*/
+   
 	
 	
 	while(fscanf(ficheros,"%s %f %d %d %d ",fuente[i].parametros,&fuente[i].PH,&fuente[i].conductividad,&fuente[i].turbidez,&fuente[i].coliformes) !=EOF){//bUCLE HASTA el fin del fichero,cuando no sabes su tamañ
         
 		
-		   //printf("%s\t%f\t%d\t%d\t%d\t \n",fuente[i].parametros,fuente[i].PH,fuente[i].conductividad,fuente[i].turbidez,fuente[i].coliformes);
 		   
-		  if( fuente[i].coliformes<1 && (fuente[i].PH>6.5 && fuente[i].PH<9.5) && (fuente[i].conductividad >50 && fuente[i].conductividad <500) && fuente[i].turbidez<1)// fuente[i].coliformes<0 && fuente[i].PH>6.5 && fuente[i].PH<9.5 && (fuente[i].conductividad >50 && fuente[i].conductividad <500) && fuente[i].turbidez<1
+		   
+		  
+		
+		   i++;
+		}
+		fclose(ficheros);
+		
+		
+		
+		
+		
+		if( fuente[i].coliformes<1 && (fuente[i].PH>6.5 && fuente[i].PH<9.5) && (fuente[i].conductividad >50 && fuente[i].conductividad <500) && fuente[i].turbidez<1)// fuente[i].coliformes<0 && fuente[i].PH>6.5 && fuente[i].PH<9.5 && (fuente[i].conductividad >50 && fuente[i].conductividad <500) && fuente[i].turbidez<1
         {
         	potable++;
         	printf("%s\t%.2f\t%d\t%d\t%d\t \n",fuente[i].parametros,fuente[i].PH,fuente[i].conductividad,fuente[i].turbidez,fuente[i].coliformes);
 		   
 		   
         	mediaPh=mediaPh + fuente[i].PH;
-		}
-		
-		   i++;
 		}
 	printf("Hay una cantidad de agua potable de :%d \n",potable);
 	porcentaje = (float)potable/ NumerodFuentes;
@@ -134,6 +87,6 @@ float porcentaje;
 	
 	
 	
-	fclose(ficheros);
+	
 return 0;	
 }
